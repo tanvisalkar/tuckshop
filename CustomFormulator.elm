1c2803a049834dd75ed51bd663a4cce8170bf3c7
tanvi salkar <tanvi.s@ajency.in>
2018-08-24 11:40:25 +0530

    email field added

--- CustomFormulator.elm
+++ CustomFormulator.elm
@@ -2,6 +2,8 @@ module CustomFormulator exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (id, type_, name,style,class,for)
 
+
+
 type alias InputField =
     { slug : String
     ,   displayLabel : String
@@ -11,6 +13,24 @@ type alias InputField =
     ,   isHidden : Bool
     ,   displayOrder : Int
     ,   optionalAttrib : AttributeType
+    }
+
+type alias TextAreaField =
+     { slug : String
+    ,   displayLabel : String
+    ,   placeHolderText : String
+    ,   helpText : String
+    ,   isOptional : Bool
+    ,   isHidden : Bool
+    ,   displayOrder : Int
+    ,   optionalAttrib : AttributeType
+    }
+
+type alias CombinationField = 
+    {
+        inputfields : List InputField
+    ,   textareafields : List TextAreaField
+
     }
 
 type AttributeType = Text 
@@ -36,9 +56,7 @@ type alias Form =
     }
 
 type alias Model =
-    { nicknames : List String
-    , errorMessage : Maybe String
-    , formElem : List SubElement
+    { formElem : List SubElement
     }
 
 formval : SubElement
@@ -60,11 +78,28 @@ formval =
             }]
         }
 
+formEmailVal : SubElement
+formEmailVal = 
+    { displayLabel = "Email"
+        ,   helpText = ""
+        ,   isOptional = False
+        ,   isHidden = False
+        ,   displayOrder = 1
+        ,   optionalAttrib = Text
+        ,   subElements =  [{ slug = "email"
+                ,   displayLabel = ""
+                ,   placeHolderText = "Enter email"
+                ,   helpText = ""
+                ,   isOptional = False
+                ,   isHidden = False
+                ,   displayOrder = 1
+                ,   optionalAttrib = Email
+            }]
+        }
+
 init : ( Model, Cmd msg )
 init =
-    ( { nicknames = []
-      , errorMessage = Nothing
-      , formElem = [ formval ]
+    ( { formElem = [ formval, formEmailVal ]
       }
     , Cmd.none
     )
@@ -74,53 +109,68 @@ update msg model =
     (model,Cmd.none)
 
 
-generateFields: SubElement -> Html msg
-generateFields subElement=
-    case subElement.optionalAttrib of
+generateField : InputField -> Html msg
+generateField element =
+    case element.optionalAttrib of
         Text ->
             input [ class "form-control"
-                            , id "name"
+                            , id element.slug
                             , type_ "text"
                             ]
                             [] 
         Email  ->
             input [ class "form-control"
-                            , id "name"
+                            , id element.slug
                             , type_ "email"
                             ]
                             [] 
         Tel ->
             input [ class "form-control"
-                            , id "name"
+                            , id element.slug
                             , type_ "tel"
                             ]
                             [] 
         Number ->
             input [ class "form-control"
-                            , id "name"
+                            , id element.slug
                             , type_ "number"
                             ]
                             [] 
 
+generateFields : List InputField -> List  (Html msg) -> List (Html msg)
+generateFields lstField lst =
+    case lstField of
+        [] ->   lst
+        (x::xs) -> 
+            List.append lst [(generateField x)]
+
 -- takes each form input and returns list of html
 generateRoot : SubElement -> Html msg  
 generateRoot field = 
-    div []
-    [
-        label [  for "name" ]
-        [ text "Name" ]
-    ,   generateFields field
-    ]
+    let 
+        labelComp = [ label [  for field.displayLabel ]
+                    [ text field.displayLabel ]
+                    ]
+    in 
+        let
+            formComp = 
+                if List.length field.subElements > 0 then
+                    List.append labelComp (generateFields field.subElements [])
+                else 
+                    labelComp
+        in
+            div []
+                formComp
     
 
 -- takes each form input and returns list of html
-generateForm : List SubElement -> Html msg  
-generateForm lst = 
-    case lst of
-        [] ->   div [] [text "test"]
+generateForm : List SubElement -> List (Html msg) -> Html msg  
+generateForm lst1 lst2 = 
+    case lst1 of
+        [] ->   div [] lst2
         (x::xs) -> 
             -- div [] [text "test"]
-            generateRoot x
+            generateForm  xs (List.append lst2 [generateRoot x])
 
 
 
@@ -129,7 +179,7 @@ view : Model -> Html msg
 view model =
     div []
         [
-            generateForm model.formElem
+            generateForm model.formElem []
         ]
 
 
